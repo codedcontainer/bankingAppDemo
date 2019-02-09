55c501ac55e137b7a2c1da1d4a1ef87eb221a988
Jason Utt <jdmagic21@gmail.com>
2019-02-09 11:26:14 -0500

    dotnet core banking app init

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using banking;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bank bank = new bank();
+            bank.accounts = new List<accountee>(){
+                new accountee()
+                {
+                    name = "Alice",
+                    checking = new Checking(1000.00),
+                    savings = new Savings(5000)
+                },
+                new accountee()
+                {
+                    name = "Bob",
+                    checking = new Checking(500.00),
+                    savings = new Savings(2000.00)
+                },
+                new accountee()
+            {
+                name = "Eve",
+                checking = new Checking(5000)
+            }
+            };
+
+            accountee alice = (from account in bank.accounts
+                               where account.name == "Alice"
+                               select account).First();
+
+            alice.checking.withdrawal(500.00);
+            alice.checking.deposit(2000.00);
+            alice.checking.accountTransfer(1500.00, ref alice, "savings");
+            alice.savings.setInterest(12);
+
+            accountee bob = (from account in bank.accounts
+                             where account.name == "Bob"
+                             select account).First();
+
+            bob.checking.deposit(1500);
+            bob.savings.withdrawal(1000);
+            bob.savings.setInterest(12);
+
+            accountee eve = (from account in bank.accounts
+                             where account.name == "Eve"
+                             select account).First();
+
+            eve.checking.deposit(2000);
+            eve.checking.withdrawal(1000);
+
+            foreach (var account in bank.accounts)
+            {
+
+                Console.WriteLine(
+                    "Account Owner - {0} \t\n Savings Amount - {1} \t\n Checking Amount - {2}",
+                    account.name,
+                    account.savings != null ? account.savings.getBalance() : 0,
+                    account.checking != null ? account.checking.getBalance() : 0
+                );
+            }
+
+        }
+    }
+}
---
+++ bank.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+
+namespace banking
+{
+     class Account
+    {
+        double maxWithdrawal = 500.00; 
+        double interest = .001;
+        private double accountBalance {get;set;}
+        public double getBalance(){
+            return this.accountBalance; 
+        }
+        public void setBalance(double amount){
+            this.accountBalance = amount; 
+        }
+        public void withdrawal(double amount){
+            if (!( amount > maxWithdrawal) ){
+                this.accountBalance -= amount;
+            }
+        }
+        public void deposit(double amount){
+            this.accountBalance += amount;
+        }
+        public void setInterest(int multiplyer){
+            this.accountBalance = this.accountBalance + (this.accountBalance * ( this.interest * multiplyer) );
+        }
+        public void accountTransfer(double amount, ref accountee accountHolder, string accountType){
+            this.accountBalance -= amount;
+            if (accountType == "savings"){
+                accountHolder.savings.accountBalance += amount; 
+            }
+            else{
+                accountHolder.checking.accountBalance += amount; 
+            }
+        }
+
+    }
+    class Checking: Account{
+        public Checking(double initAmount){
+            this.setBalance(initAmount); 
+        }
+    }
+    class Savings: Account{
+        public Savings(double initAmount){
+            this.setBalance(initAmount);
+        }
+    }
+
+    class bank{
+        public List<accountee> accounts {get; set; }
+    }
+    class accountee{
+       public string name {get; set;}
+       public Savings savings {get; set;}
+       public Checking checking {get; set;}
+    }
+}
